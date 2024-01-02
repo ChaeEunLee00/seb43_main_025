b42ac8f5fbb4b413461995ecbb256fb4824980aa
gtaegyeong <gangtaegyeong12@gmail.com>
2024-01-02 14:20:44 +0900

    feat : 필터체인 원상복구

--- backend/src/main/java/com/codestates/auth/config/SecurityConfiguration.java
+++ backend/src/main/java/com/codestates/auth/config/SecurityConfiguration.java
@@ -45,9 +45,9 @@ public class SecurityConfiguration {
                 .headers().frameOptions().sameOrigin()
                 .and()
                 .csrf().disable()
-                //.cors(Customizer.withDefaults())
-                .cors().configurationSource(corsConfigurationSource())
-                .and()
+                .cors(Customizer.withDefaults())
+                //.cors().configurationSource(corsConfigurationSource())
+                //.and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
                 .and()
@@ -85,7 +85,7 @@ public class SecurityConfiguration {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration cors = new CorsConfiguration();
         cors.setAllowCredentials(true);
-        cors.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://thegong.site", "https://thegong.site", "https://seb43-main-025.vercel.app", "https://www.apithegong.com"));
+        cors.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://thegong.site", "https://thegong.site", "https://seb43-main-025.vercel.app", "http://www.apithegong.com", "https://www.apithegong.com"));
         cors.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE", "OPTIONS"));
         cors.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
