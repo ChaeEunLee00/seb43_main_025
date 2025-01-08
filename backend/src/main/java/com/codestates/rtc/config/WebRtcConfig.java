fb25657d4ebc0975d7c555c760cb86cb1957961c
ChaeEunLee00 <ichaeeun@ichaeeun-ui-MacBookPro.local>
2025-01-08 11:25:56 +0900

    KMS 재실행2

--- backend/src/main/java/com/codestates/rtc/config/WebRtcConfig.java
+++ backend/src/main/java/com/codestates/rtc/config/WebRtcConfig.java
@@ -30,7 +30,7 @@ public class WebRtcConfig implements WebSocketConfigurer {
 
     @Bean
     public KurentoClient kurentoClient() {
-        return KurentoClient.create("ws://43.201.251.71:8888/kurento");
+        return KurentoClient.create("ws://3.38.101.206:8888/kurento");
     } //test
 
     @Bean
